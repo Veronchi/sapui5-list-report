dd36bfb7b0d4a80db6a4a51347af411a9808becc
Veronchi <zhigunova1995@gmail.com>
2024-03-05 14:40:04 +0300

    implement unit tests for delete text formatter

---
+++ project/webapp/test/unit/model/formatter.js
@@ -0,0 +1,38 @@
+sap.ui.define(["veronchi/leverx/project/model/formatter", "sap/ui/model/resource/ResourceModel"], function (formatter, ResourceModel) {
+  "use strict";
+
+  function getResourceBundle() {
+    return new ResourceModel({
+      bundleUrl: [sap.ui.require.toUrl("veronchi/leverx/project"), "i18n/i18n.properties"].join("/")
+    }).getResourceBundle();
+  }
+
+  QUnit.module("Formatting functions", {
+    beforeEach() {
+      this.oRresourceBundle = getResourceBundle();
+      formatter.initResourceBundle(this.oRresourceBundle);
+    }
+  });
+
+  QUnit.test("Should return text with the name of the product that will be deleted", function (assert) {
+    const aOneProduct = ["MacBook Air"];
+    const sResultForOneProduct = formatter.formatConfirmMessageText(aOneProduct);
+
+    assert.strictEqual(
+      sResultForOneProduct,
+      this.oRresourceBundle.getText("ConfirmDeleteProductText", [aOneProduct[0]]),
+      sResultForOneProduct
+    );
+  });
+
+  QUnit.test("Should return text with the number of products to be deleted", function (assert) {
+    const aMultipleProducts = ["MacBook Air", "MacBook Pro 14"];
+    const sResultForMultipleProducts = formatter.formatConfirmMessageText(aMultipleProducts);
+
+    assert.strictEqual(
+      sResultForMultipleProducts,
+      this.oRresourceBundle.getText("ConfirmDeleteProductsText", [aMultipleProducts.length]),
+      sResultForMultipleProducts
+    );
+  });
+});
