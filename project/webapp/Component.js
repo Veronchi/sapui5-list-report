a18d04eb511508e171b9a600dadb41f6e13ee302
Veronchi <zhigunova1995@gmail.com>
2024-03-24 22:50:38 +0300

    add JSDoc

--- project/webapp/Component.js
+++ project/webapp/Component.js
@@ -3,10 +3,27 @@ sap.ui.define(
   function (UIComponent, productModel, constants) {
     "use strict";
 
+    /**
+     * The component controller provides the metadata and the component methods
+     *
+     * @extends sap.ui.core.UIComponent
+     * @namespace veronchi.leverx.project.Component
+     */
+
     return UIComponent.extend("veronchi.leverx.project.Component", {
       metadata: {
         manifest: "json"
       },
+
+      /**
+       * It calls the `init` method of the base class (`UIComponent`) using the `apply` function to ensure proper initialization.
+       * After initializing the base class, it also initializes the component's router and Product Model.
+       *
+       * @function
+       * @name init
+       * @memberof veronchi.leverx.project.Component
+       * @returns {void} - No return value.
+       */
 
       init: function () {
         UIComponent.prototype.init.apply(this, arguments);
